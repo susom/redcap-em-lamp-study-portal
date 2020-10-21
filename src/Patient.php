83c8554b9439f92ba467eddc1c2587257a436ba5
Jordan Schultz <jmschultz@ucdavis.edu>
2020-10-21 12:44:43 -0700

    added constants for translation between pattern and redcap naming schemes, set patient json

--- src/Patient.php
+++ src/Patient.php
@@ -11,50 +11,27 @@ use \GuzzleHttp\Client;
  */
 class Patient
 {
-    /** @var string $uuid */
-    private $uuid;
+    /**
+     * const variables
+     */
+    const uuid = 'patient_uuid';
+    const email = 'patient_email';
+    const mobileNumber = 'patient_mobile_number';
+    const firstName = 'patient_first_name';
+    const lastName = 'patient_last_name';
+    const timeZone = 'patient_time_zone';
+    const locale = 'patient_locale';
+    const gender = 'patient_gender';
+    const inHospital = 'patient_in_hospital';
+    const birthDate = 'patient_birth_date';
+    const accountStatus = 'patient_account_status';
+    const lastLoginTime = 'patient_last_login';
+    const locked = 'patient_locked';
+    const verified = 'patient_verified';
+    const mobileNumberVerified = 'patient_mobile_verified';
 
-    /** @var string $email */
-    private $email;
-
-    /** @var string $mobile_number */
-    private $mobile_number;
-
-    /** @var string $first_name */
-    private $first_name;
-
-    /** @var string $last_name */
-    private $last_name;
-
-    /** @var string $time_zone */
-    private $time_zone;
-
-    /** @var string $locale */
-    private $locale;
-
-    /** @var string $gender */
-    private $gender;
-
-    /** @var boolean $in_hospital */
-    private $in_hospital;
-
-    /** @var string $birth_date */
-    private $birth_date;
-
-    /** @var string $account_status */
-    private $account_status;
-
-    /** @var string $last_login_time */
-    private $last_login_time;
-
-    /** @var booleann $locked */
-    private $locked;
-
-    /** @var boolean $verified */
-    private $verified;
-
-    /** @var string $mobile_number_verified */
-    private $mobile_number_verified;
+    /** @var arr $patient_json */
+    private $patient_json;
 
     /** @var array $tasks */
     private $tasks;
@@ -62,17 +39,22 @@ class Patient
     /** @var Client $client */
     private $client;
 
-
     /**
      * Patient constructor.
+     * @param $client
+     * @param $patient_json
      */
-    public function __construct($client, $uuid)
+    public function __construct($client, $patient_json)
     {
-        $this->setUuid($uuid);
         $this->setClient($client);
-
+        $this->setPatientJson($patient_json);
         $this->processTasks();
+    }
 
+    public function getConstants()
+    {
+        $refl = new \ReflectionClass($this);
+        return $refl->getConstants();
     }
 
     private function processTasks()
@@ -89,269 +71,23 @@ class Patient
 
     }
 
-    /**
-     * @return string
-     */
-    public function getUuid()
+    public function getAttributes()
     {
-        return $this->uuid;
-    }
-
-    /**
-     * @param string $uuid
-     */
-    public function setUuid($uuid)
-    {
-        $this->uuid = $uuid;
-    }
-
-    /**
-     * @return string
-     */
-    public function getEmail()
-    {
-        return $this->email;
-    }
-
-    /**
-     * @param string $email
-     */
-    public function setEmail($email)
-    {
-        $this->email = $email;
-    }
-
-    /**
-     * @return string
-     */
-    public function getMobileNumber()
-    {
-        return $this->mobile_number;
-    }
-
-    /**
-     * @param string $mobile_number
-     */
-    public function setMobileNumber($mobile_number)
-    {
-        $this->mobile_number = $mobile_number;
-    }
-
-    /**
-     * @return string
-     */
-    public function getFirstName()
-    {
-        return $this->first_name;
-    }
-
-    /**
-     * @param string $first_name
-     */
-    public function setFirstName($first_name)
-    {
-        $this->first_name = $first_name;
-    }
-
-    /**
-     * @return string
-     */
-    public function getLastName()
-    {
-        return $this->last_name;
-    }
-
-    /**
-     * @param string $last_name
-     */
-    public function setLastName($last_name)
-    {
-        $this->last_name = $last_name;
-    }
-
-    /**
-     * @return string
-     */
-    public function getTimeZone()
-    {
-        return $this->time_zone;
-    }
-
-    /**
-     * @param string $time_zone
-     */
-    public function setTimeZone($time_zone)
-    {
-        $this->time_zone = $time_zone;
-    }
-
-    /**
-     * @return string
-     */
-    public function getLocale()
-    {
-        return $this->locale;
-    }
-
-    /**
-     * @param string $locale
-     */
-    public function setLocale($locale)
-    {
-        $this->locale = $locale;
-    }
-
-    /**
-     * @return string
-     */
-    public function getGender()
-    {
-        return $this->gender;
-    }
-
-    /**
-     * @param string $gender
-     */
-    public function setGender($gender)
-    {
-        $this->gender = $gender;
-    }
-
-    /**
-     * @return bool
-     */
-    public function isInHospital()
-    {
-        return $this->in_hospital;
-    }
-
-    /**
-     * @param bool $in_hospital
-     */
-    public function setInHospital($in_hospital)
-    {
-        $this->in_hospital = $in_hospital;
-    }
-
-    /**
-     * @return string
-     */
-    public function getBirthDate()
-    {
-        return $this->birth_date;
-    }
-
-    /**
-     * @param string $birth_date
-     */
-    public function setBirthDate($birth_date)
-    {
-        $this->birth_date = $birth_date;
-    }
-
-    /**
-     * @return string
-     */
-    public function getAccountStatus()
-    {
-        return $this->account_status;
-    }
-
-    /**
-     * @param string $account_status
-     */
-    public function setAccountStatus($account_status)
-    {
-        $this->account_status = $account_status;
-    }
-
-    /**
-     * @return string
-     */
-    public function getLastLoginTime()
-    {
-        return $this->last_login_time;
-    }
-
-    /**
-     * @param string $last_login_time
-     */
-    public function setLastLoginTime($last_login_time)
-    {
-        $this->last_login_time = $last_login_time;
-    }
-
-    /**
-     * @return booleann
-     */
-    public function getLocked()
-    {
-        return $this->locked;
-    }
-
-    /**
-     * @param booleann $locked
-     */
-    public function setLocked($locked)
-    {
-        $this->locked = $locked;
-    }
-
-    /**
-     * @return bool
-     */
-    public function isVerified()
-    {
-        return $this->verified;
-    }
-
-    /**
-     * @param bool $verified
-     */
-    public function setVerified($verified)
-    {
-        $this->verified = $verified;
-    }
-
-    /**
-     * @return string
-     */
-    public function getMobileNumberVerified()
-    {
-        return $this->mobile_number_verified;
-    }
-
-    /**
-     * @param string $mobile_number_verified
-     */
-    public function setMobileNumberVerified($mobile_number_verified)
-    {
-        $this->mobile_number_verified = $mobile_number_verified;
-    }
-
-    /**
-     * @return array
-     */
-    public function getTasks()
-    {
-        if (!$this->tasks) {
-            $this->setTasks();
+        $attributes = [];
+        foreach ($this as $key => $val) {
+            array_push($attributes,$val);
         }
-        return $this->tasks;
+        return $attributes;
     }
 
-    /**
-     * @param array $tasks
-     */
-    public function setTasks($tasks = array())
+    public function getPatientJson()
     {
-        // first time just make api call to get the tasks for this patient otherwise update existing ones.
-        if (empty($tasks)) {
-            $this->tasks = $this->getClient()->request('get', BASE_PATTERN_HEALTH_API_URL . 'users/' . $this->getUuid() . '/tasks');;
-        } else {
-            $this->tasks = $tasks;
-        }
+        return $this->patient_json;
+    }
 
+    public function setPatientJson($patient_json)
+    {
+        $this->patient_json=$patient_json;
     }
 
     /**
@@ -370,5 +106,31 @@ class Patient
         $this->client = $client;
     }
 
+    /**
+     * @return array
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function getTasks()
+    {
+        if (!$this->tasks) {
+            $this->setTasks();
+        }
+        return $this->tasks;
+    }
 
+    /**
+     * @param array $tasks
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function setTasks($tasks = array())
+    {
+        $uuid = $this->getPatientJson()['user']['uuid'];
+
+        // first time just make api call to get the tasks for this patient otherwise update existing ones.
+        if (empty($tasks)) {
+            $this->tasks = $this->getClient()->request('get', BASE_PATTERN_HEALTH_API_URL . 'users/' . $uuid . '/tasks');
+        } else {
+            $this->tasks = $tasks;
+        }
+    }
 }
