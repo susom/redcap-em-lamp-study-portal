d94019426eb2c1ba22822d9f11be63d299893689
Jordan Schultz <jmschultz@ucdavis.edu>
2020-12-09 10:54:34 -0800

    added simple text to resultant page upon finish

--- src/workflow/cronImageScanner.php
+++ src/workflow/cronImageScanner.php
@@ -10,5 +10,15 @@ $type = $module->getProjectSetting("workflow");
 if($type == "image_adjudication"){
     $module->initialize();
     $module->emLog("Workflow: " . $type . " Duration of run: " . (microtime(true) - $startTS));
+
 }
 
+?>
+
+<nav class="navbar navbar-light bg-light">
+    <span class="navbar-text">
+        Image scan finished, please check record status dashboard for results
+        <br>
+        <strong>Duration of run: <?php echo (microtime(true) - $startTS) ?></strong>
+    </span>
+</nav>
--- src/workflow/dataImportScanner.php
+++ src/workflow/dataImportScanner.php
@@ -12,3 +12,13 @@ if ($type == "lazy_import") {
     $module->emLog("Workflow: " . $type . " Duration of run: " . (microtime(true) - $startTS));
 }
 
+?>
+
+<nav class="navbar navbar-light bg-light">
+    <span class="navbar-text">
+        Manual data scan finished, please check record status dashboard for results
+        <br>
+        <strong>Duration of run: <?php echo (microtime(true) - $startTS) ?></strong>
+    </span>
+</nav>
+
